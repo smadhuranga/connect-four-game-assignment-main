ea157d031fd9f4dc6cfd0ec44e0bad1aff217627
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2024-03-17 10:26:10 +0530

    course work oop

---
+++ src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -0,0 +1,109 @@
+package lk.ijse.dep.service;
+
+public class BoardImpl implements Board {
+
+    private final Piece [][] pieces;
+
+    private final BoardUI boardUI;
+
+    public BoardImpl(BoardUI boardUI) {
+        this.boardUI = boardUI;
+        this.pieces=new Piece[6][5];
+
+        //initialize all the pieces
+        for (int i = 0; i < pieces.length; i++) {
+            for (int j = 0; j < pieces[i].length; j++) {
+                pieces[i][j]=Piece.EMPTY;
+            }
+        }
+    }
+    public Piece[][] getPieces() {
+        return pieces;
+    }
+    public BoardUI getBoardUI() {
+        return this.boardUI;
+    }
+
+    @Override
+    public int findNextAvailableSpot(int col) {
+
+        for (int i = 0; i < pieces[col].length; i++) {
+            if (pieces[col][i]==Piece.EMPTY){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean isLegalMove(int col) {
+        int index=findNextAvailableSpot(col);
+        return index != -1;
+    }
+
+    @Override
+    public boolean existLegalMoves() {
+        for (int i = 0; i < pieces.length; i++) {
+            if (isLegalMove(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void updateMove(int col, Piece move) {
+        int index=findNextAvailableSpot(col);
+        pieces[col][index]=move;
+    }
+
+    @Override
+    public void updateMove(int col, int row, Piece move) {
+        pieces[col][row]=move;
+    }
+
+    @Override
+    public Winner findWinner() {
+
+        int count=0;
+
+        //Vertically
+
+        for (int i = 0; i < pieces.length; i++){
+            for (int j = 0; j < pieces[i].length-1; j++){
+                if (pieces[i][j]==pieces[i][j+1]){
+                    count++;
+                    if (count==3 && pieces[i][j]!=Piece.EMPTY){
+                        return new Winner(pieces[i][j],i,(j-2),i,(j+1));
+                    }
+                }
+                else{
+                    count=0;
+                }
+            }
+            count=0;
+        }
+
+        count=0;
+
+        //Horizontally
+
+        for (int i = 0; i < pieces[0].length; i++){
+            for (int j = 0; j < pieces.length-1; j++){
+                if (pieces[j][i]==pieces[j+1][i]){
+                    count++;
+                    if (count==3 && pieces[j][i]!=Piece.EMPTY){
+                        return  new Winner(pieces[j][i],(j-2),i,(j+1),i);
+                    }
+                }
+                else{
+                    count=0;
+                }
+            }
+            count=0;
+        }
+        return new Winner(Piece.EMPTY);
+
+    }
+
+}
